7996dfcefecb75c4e9e5614236cd274a1ad4668f
keevskipp <kevin.cruzu@uanl.edu.mx>
2025-02-05 14:02:21 -0600

    while profe repositorio

---
+++ Semana 3/if.py
@@ -0,0 +1,30 @@
+num1 =5
+num2 = 3
+
+condicion = num1 > num2
+
+cond = bool(none) #si pones 0 o 0.000 o "none", da falso lo demas es true
+print(condicion)
+if condicion:
+    print("si")
+
+if cond:
+    print("accion")
+
+if cond:
+    print("verdadero")
+else 
+    print("falso")
+
+cond1 =1
+while cond1 :
+edad = int(input("captura la edad"))
+if edad < 18:
+    print("Eres menor")
+elif edad>=18
+    print("eres mayor")
+elif edad >=60 and edad <=90
+    print("saca pension")
+else
+    print("ya puedes usar legos")
+    cond1 = false
